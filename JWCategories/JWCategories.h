1697b8fddf2cd67e78366758b850b12c00d4e041
Jason Whitehorn <jason.whitehorn@gmail.com>
2012-12-19 21:47:46 -0600

    initial commit

---
+++ JWCategories/JWCategories.h
@@ -0,0 +1,13 @@
+//
+//  JWCategories.h
+//  JWCategories
+//
+//  Created by Jason Whitehorn on 12/19/12.
+//  Copyright (c) 2012 Jason Whitehorn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JWCategories : NSObject
+
+@end
---
+++ JWCategories/JWCategories.m
@@ -0,0 +1,13 @@
+//
+//  JWCategories.m
+//  JWCategories
+//
+//  Created by Jason Whitehorn on 12/19/12.
+//  Copyright (c) 2012 Jason Whitehorn. All rights reserved.
+//
+
+#import "JWCategories.h"
+
+@implementation JWCategories
+
+@end
